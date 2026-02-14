b6a32e131b4d5481ab7086b497ed5f36b8bd2b2a
allivilsey <allivilsey@outlook.com>
2026-02-14 22:06:57 +0800

    更新

--- src/main/java/org/allivilsey/youmuchan/AIContext.java
+++ src/main/java/org/allivilsey/youmuchan/AIContext.java
@@ -1,24 +1,52 @@
 package org.allivilsey.youmuchan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AIContext {
 
-    private final String model;
-    private final Float temperature;
+    //模型名称
+    private String model;
+    //模型温度
+    private Float temperature;
+    //本次请求创建时间
+    private long requestTime = System.currentTimeMillis();
 
+    //系统|用户提示词
     private String systemPrompt;
     private String userPrompt;
+
+    //未处理原始游戏数据
+    private List<InGameInfo> rawInfos = new ArrayList<>();
+    //已过滤游戏数据
+    private List<InGameInfo> filteredInfos = new ArrayList<>();
+
+    //模型检测是否为注入攻击（默认否）
+    private boolean injectionRisk = false;
+    //模型判断应对情绪
+    private String emotion = "NEUTRAL";
+    //模型判断wiki使用需求
+    private boolean wikiRequired = false;
+
+    //目标玩家
+    private String targetPlayer;
 
     public AIContext(String model, Float temperature) {
         this.model = model;
         this.temperature = temperature;
     }
 
+    //GETTER
     public String getModel() {
         return model;
     }
 
     public Float getTemperature() {
         return temperature;
+    }
+
+    public long getRequestTime() {
+        return requestTime;
     }
 
     public String getSystemPrompt() {
@@ -35,5 +63,53 @@ public class AIContext {
 
     public void setUserPrompt(String userPrompt) {
         this.userPrompt = userPrompt;
+    }
+
+    public List<InGameInfo> getRawInfos() {
+        return rawInfos;
+    }
+
+    public void setRawInfos(List<InGameInfo> rawInfos) {
+        this.rawInfos = rawInfos;
+    }
+
+    public List<InGameInfo> getFilteredInfos() {
+        return filteredInfos;
+    }
+
+    public void setFilteredInfos(List<InGameInfo> filteredInfos) {
+        this.filteredInfos = filteredInfos;
+    }
+
+    public boolean isInjectionRisk() {
+        return injectionRisk;
+    }
+
+    public void setInjectionRisk(boolean injectionRisk) {
+        this.injectionRisk = injectionRisk;
+    }
+
+    public String getEmotion() {
+        return emotion;
+    }
+
+    public void setEmotion(String emotion) {
+        this.emotion = emotion;
+    }
+
+    public boolean isWikiRequired() {
+        return wikiRequired;
+    }
+
+    public void setWikiRequired(boolean wikiRequired) {
+        this.wikiRequired = wikiRequired;
+    }
+
+    public String getTargetPlayer() {
+        return targetPlayer;
+    }
+
+    public void setTargetPlayer(String targetPlayer) {
+        this.targetPlayer = targetPlayer;
     }
 }
